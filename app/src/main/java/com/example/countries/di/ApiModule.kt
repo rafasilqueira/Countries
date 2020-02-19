f9a2fa58fc01d90aed42f6761ef91c1e6aa3520a
rafasilqueira <rafasilqueira@hotmail.com>
2020-02-18 21:12:55 -0300

    More tests

--- app/src/main/java/com/example/countries/di/ApiModule.kt
+++ app/src/main/java/com/example/countries/di/ApiModule.kt
@@ -1,25 +1,53 @@
 package com.example.countries.di
 
+import com.example.countries.BuildConfig
 import com.example.countries.model.CountriesAPI
 import com.example.countries.model.CountryService
 import dagger.Module
 import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
 
 const val BASE_URL = "https://raw.githubusercontent.com"
 
 @Module
 class ApiModule {
 
+    private val loggingInterceptor = HttpLoggingInterceptor()
+
+    private val requestInterceptor = Interceptor { chain ->
+        val newRequest = chain.request().newBuilder()
+            //.addHeader("Authorization", "NA-DISP-AUTH $data")
+            .addHeader("FullObject", "1")
+            .addHeader("Timezone", TimeZone.getDefault().id)
+            .build()
+        chain.proceed(newRequest)
+    }
+
+    private val client: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(requestInterceptor)
+        .addInterceptor(loggingInterceptor).build()
+
     @Provides
-    fun providesCountriesApi(): CountriesAPI = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .build()
-        .create(CountriesAPI::class.java)
+    fun providesCountriesApi(): CountriesAPI {
+
+        loggingInterceptor.level =
+            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(CountriesAPI::class.java)
+    }
 
     @Provides
     fun providesCountriesService(): CountryService = CountryService()
--- app/src/main/java/com/example/countries/view/MainActivity.kt
+++ app/src/main/java/com/example/countries/view/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.countries.view
 
+import android.content.Context
+import android.content.ContextWrapper
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.util.Base64
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +17,16 @@ import com.example.countries.R
 import com.example.countries.adapter.CountryAdapter
 import com.example.countries.viewmodel.CountryListViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
+
+
+const val FOLDER_NAME = "juca"
+const val BASE64_EXAMPLE =
+    "R0lGODlhPQBEAPeoAJosM//AwO/AwHVYZ/z595kzAP/s7P+goOXMv8+fhw/v739/f+8PD98fH/8mJl+fn/9ZWb8/PzWlwv///6wWGbImAPgTEMImIN9gUFCEm/gDALULDN8PAD6atYdCTX9gUNKlj8wZAKUsAOzZz+UMAOsJAP/Z2ccMDA8PD/95eX5NWvsJCOVNQPtfX/8zM8+QePLl38MGBr8JCP+zs9myn/8GBqwpAP/GxgwJCPny78lzYLgjAJ8vAP9fX/+MjMUcAN8zM/9wcM8ZGcATEL+QePdZWf/29uc/P9cmJu9MTDImIN+/r7+/vz8/P8VNQGNugV8AAF9fX8swMNgTAFlDOICAgPNSUnNWSMQ5MBAQEJE3QPIGAM9AQMqGcG9vb6MhJsEdGM8vLx8fH98AANIWAMuQeL8fABkTEPPQ0OM5OSYdGFl5jo+Pj/+pqcsTE78wMFNGQLYmID4dGPvd3UBAQJmTkP+8vH9QUK+vr8ZWSHpzcJMmILdwcLOGcHRQUHxwcK9PT9DQ0O/v70w5MLypoG8wKOuwsP/g4P/Q0IcwKEswKMl8aJ9fX2xjdOtGRs/Pz+Dg4GImIP8gIH0sKEAwKKmTiKZ8aB/f39Wsl+LFt8dgUE9PT5x5aHBwcP+AgP+WltdgYMyZfyywz78AAAAAAAD///8AAP9mZv///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAKgALAAAAAA9AEQAAAj/AFEJHEiwoMGDCBMqXMiwocAbBww4nEhxoYkUpzJGrMixogkfGUNqlNixJEIDB0SqHGmyJSojM1bKZOmyop0gM3Oe2liTISKMOoPy7GnwY9CjIYcSRYm0aVKSLmE6nfq05QycVLPuhDrxBlCtYJUqNAq2bNWEBj6ZXRuyxZyDRtqwnXvkhACDV+euTeJm1Ki7A73qNWtFiF+/gA95Gly2CJLDhwEHMOUAAuOpLYDEgBxZ4GRTlC1fDnpkM+fOqD6DDj1aZpITp0dtGCDhr+fVuCu3zlg49ijaokTZTo27uG7Gjn2P+hI8+PDPERoUB318bWbfAJ5sUNFcuGRTYUqV/3ogfXp1rWlMc6awJjiAAd2fm4ogXjz56aypOoIde4OE5u/F9x199dlXnnGiHZWEYbGpsAEA3QXYnHwEFliKAgswgJ8LPeiUXGwedCAKABACCN+EA1pYIIYaFlcDhytd51sGAJbo3onOpajiihlO92KHGaUXGwWjUBChjSPiWJuOO/LYIm4v1tXfE6J4gCSJEZ7YgRYUNrkji9P55sF/ogxw5ZkSqIDaZBV6aSGYq/lGZplndkckZ98xoICbTcIJGQAZcNmdmUc210hs35nCyJ58fgmIKX5RQGOZowxaZwYA+JaoKQwswGijBV4C6SiTUmpphMspJx9unX4KaimjDv9aaXOEBteBqmuuxgEHoLX6Kqx+yXqqBANsgCtit4FWQAEkrNbpq7HSOmtwag5w57GrmlJBASEU18ADjUYb3ADTinIttsgSB1oJFfA63bduimuqKB1keqwUhoCSK374wbujvOSu4QG6UvxBRydcpKsav++Ca6G8A6Pr1x2kVMyHwsVxUALDq/krnrhPSOzXG1lUTIoffqGR7Goi2MAxbv6O2kEG56I7CSlRsEFKFVyovDJoIRTg7sugNRDGqCJzJgcKE0ywc0ELm6KBCCJo8DIPFeCWNGcyqNFE06ToAfV0HBRgxsvLThHn1oddQMrXj5DyAQgjEHSAJMWZwS3HPxT/QMbabI/iBCliMLEJKX2EEkomBAUCxRi42VDADxyTYDVogV+wSChqmKxEKCDAYFDFj4OmwbY7bDGdBhtrnTQYOigeChUmc1K3QTnAUfEgGFgAWt88hKA6aCRIXhxnQ1yg3BCayK44EWdkUQcBByEQChFXfCB776aQsG0BIlQgQgE8qO26X1h8cEUep8ngRBnOy74E9QgRgEAC8SvOfQkh7FDBDmS43PmGoIiKUUEGkMEC/PJHgxw0xH74yx/3XnaYRJgMB8obxQW6kL9QYEJ0FIFgByfIL7/IQAlvQwEpnAC7DtLNJCKUoO/w45c44GwCXiAFB/OXAATQryUxdN4LfFiwgjCNYg+kYMIEFkCKDs6PKAIJouyGWMS1FSKJOMRB/BoIxYJIUXFUxNwoIkEKPAgCBZSQHQ1A2EWDfDEUVLyADj5AChSIQW6gu10bE/JG2VnCZGfo4R4d0sdQoBAHhPjhIB94v/wRoRKQWGRHgrhGSQJxCS+0pCZbEhAAOw=="
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,7 +36,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         swipeRefresh.setOnRefreshListener {
             swipeRefresh.isRefreshing = false
             countryListViewModel.refreshData()
@@ -33,6 +49,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         observeViewModel()
+
+        //checkImageDir()
+
+        //writeFileOnInternalStorage(this, "Leo.jpg", "Leo o melhor cachorro do mundo")
+
+        //bitmapToFile(convertString64ToImage(BASE64_EXAMPLE))
+
+        readFileFromInternalStorage()
+
     }
 
     private fun observeViewModel() {
@@ -56,7 +81,82 @@ class MainActivity : AppCompatActivity() {
                 txtError.visibility = GONE
             }
         })
+
     }
+
+    private fun readFileFromInternalStorage(){
+        val cw = ContextWrapper(this)
+        val directory = cw.getDir(FOLDER_NAME, Context.MODE_PRIVATE)
+        val value = directory.list()[2]
+        val mypath = File(directory, value)
+        Drawable.createFromPath(mypath.toString())
+    }
+
+    private fun convertString64ToImage(base64String: String): Bitmap {
+        val decodedString = Base64.decode(base64String, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
+    }
+
+    private fun bitmapToFile(bitmap: Bitmap): Uri {
+        // Get the context wrapper
+        val wrapper = ContextWrapper(applicationContext)
+
+        // Initialize a new file instance to save bitmap object
+        var file = wrapper.getDir(FOLDER_NAME, Context.MODE_PRIVATE)
+
+        file.list().forEach { println("rafael + $it") }
+        file = File(file, "${UUID.randomUUID()}.png")
+
+        try {
+            // Compress the bitmap and save in jpg format
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        // Return the saved bitmap uri
+        return Uri.parse(file.absolutePath)
+    }
+/*
+    private fun checkImageDir() {
+        val mydir: File = getDir(FOLDER_NAME, Context.MODE_PRIVATE)
+
+
+        if (!mydir.exists()) {
+            mydir.mkdirs()
+        }
+
+        *//* mydir.list().forEach {
+             println(it)
+         }*//*
+
+        val gpxfile = File("abcd", "seu dudu")
+        val writer = FileWriter(gpxfile)
+        writer.append("Fofuxo rabuxo")
+        writer.flush()
+        writer.close()
+    }
+
+    fun writeFileOnInternalStorage(mcoContext: Context, sFileName: String?, sBody: String?) {
+        val file = File(mcoContext.filesDir, FOLDER_NAME)
+
+        if (!file.exists()) {
+            file.mkdir()
+        }
+
+        try {
+            val gpxfile = File(file, sFileName)
+            val writer = FileWriter(gpxfile)
+            writer.append(sBody)
+            writer.flush()
+            writer.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }*/
 
 
 }
